9bcd78db8271df4a705e566bbb5c3da4f4a92807
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-18 16:20:55 +0100

    initial commit. Added initial project template

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,28 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+// http:websamuraj.pl/examples/js/projekt8/
+// Uzyskaj efekt jak pod tym linkiem
+// Użyj setTimeout zamiast setInterval,
+// Opóźniej start pisanie każdego słowa
+// Uwaga: Zadanie bardzo trudne
+
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = ['tekst1', 'tekst2', 'tekst3']
+
+// Implementacja
+const addLetter = () => {
+    // Użyj w środku setTimeout
+}
+
+
+addLetter(); //pierwsze wywołanie
+
+
+// Animacja kursora (zostaw bez zmian)
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active');
+}
+setInterval(cursorAnimation, 400);
