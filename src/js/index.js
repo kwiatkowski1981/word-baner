0eae01b5616aefd2633444e273f59068ea33bebd
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-18 22:11:48 +0100

    my own solution. its not very good but it works for this example :)

--- src/js/index.js
+++ src/js/index.js
@@ -16,13 +16,43 @@ console.log('HELLO 🚀')
 
 const spnText = document.querySelector('.text');
 const spnCursor = document.querySelector('.cursor');
-const txt = ['tekst1', 'tekst2', 'tekst3']
+const txt = ['tekst1', 'srekst2', 'tekst3'];
+
+const timeFirstWord = 2000;
+const timeWordsSwitch =  4800;
+const timeLetters = 600;
 
 // Implementacja
 const addLetter = () => {
     // Użyj w środku setTimeout
-}
 
+    setTimeout(() => {
+
+        for (let i = 0; i < txt.length; i++) {
+            let word = txt[i];
+
+            setTimeout(() => {
+                // console.log(word);
+            spnText.textContent = "";
+
+                for (let j = 0; j < word.length; j++) {
+
+                    setTimeout(()=> {
+                        spnText.textContent += word[j];
+                    }, j * timeLetters)
+
+                }
+
+            },  i* timeWordsSwitch);
+
+        }
+
+    }, timeFirstWord)
+
+
+
+
+}
 
 addLetter(); //pierwsze wywołanie
 
@@ -32,3 +62,12 @@ const cursorAnimation = () => {
     spnCursor.classList.toggle('active');
 }
 setInterval(cursorAnimation, 400);
+
+
+// for (let i = 1; i < 10  ; i++) {
+//         setTimeout(function timer(){
+//             console.log(i);
+//
+//         }, i * 1000);
+//
+// }
