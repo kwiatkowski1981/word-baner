4e963f6b38364345026d3671e23d2cfc7b6add6c
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-18 22:23:35 +0100

    here is it.

--- src/js/index.js
+++ src/js/index.js
@@ -18,30 +18,47 @@ const spnText = document.querySelector('.text');
 const spnCursor = document.querySelector('.cursor');
 const txt = ['tekst1', 'srekst2', 'tekst3'];
 
-const timeFirstWord = 2000;
-const timeWordsSwitch =  4800;
-const timeLetters = 600;
+let activeText = 0;
+let activeLetter = -15; //  -15 opoznia txt o 1,5sek itd
+
+// const timeFirstWord = 2000;
+// const timeWordsSwitch =  4800;
+// const timeLetters = 600;
 
 // Implementacja
 const addLetter = () => {
     // Użyj w środku setTimeout
 
-    setTimeout(() => {
-        for (let i = 0; i < txt.length; i++) {
-            let word = txt[i];
-            setTimeout(() => {
-                // console.log(word);
-            spnText.textContent = "";
-                for (let j = 0; j < word.length; j++) {
-                    setTimeout(()=> {
-                        spnText.textContent += word[j];
-                    }, j * timeLetters)
-                }
-            },  i* timeWordsSwitch);
-        }
-    }, timeFirstWord)
+    if (activeLetter >= 0) {
+        spnText.textContent += txt[activeText][activeLetter];
+    }
+    activeLetter++
+    if (activeLetter === txt[activeText].length) {
+        activeText++
+        if (activeText === txt.length) return;
 
+        return setTimeout(() => {
+            activeLetter = -15;
+            spnText.textContent = '';
+            addLetter();
+        }, 2000)
+    }
+    setTimeout(addLetter, 200)
 }
+    // setTimeout(() => {
+    //     for (let i = 0; i < txt.length; i++) {
+    //         let word = txt[i];
+    //         setTimeout(() => {
+    //             // console.log(word);
+    //         spnText.textContent = "";
+    //             for (let j = 0; j < word.length; j++) {
+    //                 setTimeout(()=> {
+    //                     spnText.textContent += word[j];
+    //                 }, j * timeLetters)
+    //             }
+    //         },  i* timeWordsSwitch);
+    //     }
+    // }, timeFirstWord)
 
 addLetter(); //pierwsze wywołanie
 
